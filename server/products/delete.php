d071ff2bd1d88185ba87e8b7bdc645f12df2e8a2
malakshaer <shaermk06@gmail.com>
2022-09-21 16:47:37 +0300

    product's delete api added

---
+++ server/products/delete.php
@@ -0,0 +1,29 @@
+<?php
+
+include("../connection.php");
+
+$id = isset($_POST["id"]);
+$name = isset($_POST["name"]);
+$description = isset($_POST["description"]);
+$price =isset($_POST["price"]);
+$discount_price = isset($_POST["discount_price"]);
+$picture_img = isset($_POST["picture_img"]);
+
+
+$query = "DELETE FROM products WHERE (id, name,description,price,discount_price,picture_img) = (?,?,?,?,?,?)";
+$query = $mysqli->prepare($query);
+$query->bind_param("ssssss", $id, $name,$description,$price,$discount_price,$picture_img);
+$query->execute();
+
+
+if($query)
+{
+  echo "done successfully!";
+
+}
+else
+{
+  echo "Something went wrong!";
+}
+
+?>
