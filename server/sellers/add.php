fd829f0bff51820964db086c046407adf6f103b4
malakshaer <shaermk06@gmail.com>
2022-09-21 20:10:38 +0300

    TASK-22 add sellers api added

--- server/sellers/add.php
+++ server/sellers/add.php
@@ -6,13 +6,14 @@ $id = isset($_POST["id"]);
 $name = isset($_POST["name"]);
 $email = isset($_POST["email"]);
 $birthday =isset($_POST["birthday"]);
+$password =isset($_POST["password"]);
 $join_date =isset($_POST["join_date"]);
 $profile_img =isset($_POST["profile_img"]);
 $seller_id = isset($_POST["seller_id"]);
 $user_type_id = isset($_POST["user_type_id"]);
 
 
-$query = "SELECT FROM users(id, name,email,birthday,join_date,profile_img) VALUE (?,?,?,?,?,?) WHERE `user_type_id` = $seller_id";
+$query = "INSERT INTO users(id, name,email,birthday,join_date,profile_img) VALUE (?,?,?,?,?,?) WHERE `user_type_id` = $seller_id";
 $query = $mysqli->prepare($query);
 $query->bind_param("ssssss", $id, $name,$email,$birthday,$join_date,$profile_img);
 $query->execute();
