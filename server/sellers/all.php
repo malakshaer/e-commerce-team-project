4ac849d86825dd8577f3f7df3d1f52f4e88234bf
malakshaer <shaermk06@gmail.com>
2022-09-21 20:16:06 +0300

    TASK-22 show all sellers api modified

--- server/sellers/all.php
+++ server/sellers/all.php
@@ -2,11 +2,11 @@
 
 include("../connection.php");
 
-$id = isset($_POST["id"]);
-$name = isset($_POST["name"]);
-$profile_img =isset($_POST["profile_img"]);
-$seller_id = isset($_POST["seller_id"]);
-$user_type_id = isset($_POST["user_type_id"]);
+$id = isset($_GET["id"]);
+$name = isset($_GET["name"]);
+$profile_img =isset($_GET["profile_img"]);
+$seller_id = isset($_GET["seller_id"]);
+$user_type_id = isset($_GET["user_type_id"]);
 
 
 $query = "SELECT FROM users(name,profile_img) = (?,?) WHERE `user_type_id` = $seller_id";
