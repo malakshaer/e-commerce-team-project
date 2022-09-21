cdc7c617d396154dec0bd4ffb7778e0aea4c4f75
malakshaer <shaermk06@gmail.com>
2022-09-21 20:13:21 +0300

    TASK-22 show all sellers api is added

--- server/sellers/all.php
+++ server/sellers/all.php
@@ -4,17 +4,14 @@ include("../connection.php");
 
 $id = isset($_POST["id"]);
 $name = isset($_POST["name"]);
-$email = isset($_POST["email"]);
-$birthday =isset($_POST["birthday"]);
-$join_date =isset($_POST["join_date"]);
 $profile_img =isset($_POST["profile_img"]);
 $seller_id = isset($_POST["seller_id"]);
 $user_type_id = isset($_POST["user_type_id"]);
 
 
-$query = "SELECT FROM users(id, name,email,birthday,join_date,profile_img) = (?,?,?,?,?,?) WHERE `user_type_id` = $seller_id";
+$query = "SELECT FROM users(name,profile_img) = (?,?) WHERE `user_type_id` = $seller_id";
 $query = $mysqli->prepare($query);
-$query->bind_param("ssssss", $id, $name,$email,$birthday,$join_date,$profile_img);
+$query->bind_param("ss", $name,$profile_img);
 $query->execute();
 
 
