e95ba45354d9ddd6b42e93e4494d84098205e268
MhamadR <mhamadraslan@gmail.com>
2022-09-21 20:07:11 +0300

    Adding categories content component :js  and adjusting its layout

---
+++ seller-frontend/components/categories-content.js
@@ -0,0 +1,80 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const categoriesContent = () => {
+    var categoriesContents = "";
+
+    sellers.map(
+      (seller) =>
+        (categoriesContents += `
+        <div class="categories-content">
+          <h2>${seller.name}</h2>
+          <h3>remove</h3>
+        </div>
+        `)
+    );
+    return categoriesContents;
+  };
+  document.getElementById("categories-content").innerHTML = categoriesContent();
+});
+
+const sellers = [
+  {
+    name: "Site Furnishings",
+  },
+  {
+    name: "Soft Flooring and Base",
+  },
+  {
+    name: "RF Shielding",
+  },
+  {
+    name: "Drilled Shafts",
+  },
+  {
+    name: "Roofing (Metal)",
+  },
+  {
+    name: "Fire Sprinkler System",
+  },
+  {
+    name: "Temp Fencing, Decorative Fencing and Gates",
+  },
+  {
+    name: "Drywall & Acoustical (MOB)",
+  },
+  {
+    name: "Landscaping & Irrigation",
+  },
+  {
+    name: "Doors, Frames & Hardware",
+  },
+  {
+    name: "Roofing (Asphalt)",
+  },
+  {
+    name: "Casework",
+  },
+  {
+    name: "Overhead Doors",
+  },
+  {
+    name: "Exterior Signage",
+  },
+  {
+    name: "Drywall & Acoustical (MOB)",
+  },
+  {
+    name: "Elevator",
+  },
+  {
+    name: "Sitework & Site Utilities",
+  },
+  {
+    name: "Doors, Frames & Hardware",
+  },
+  {
+    name: "Temp Fencing, Decorative Fencing and Gates",
+  },
+  {
+    name: "Rebar & Wire Mesh Install",
+  },
+];
