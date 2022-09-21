b92287737a4031ad1bbcb700c46f82e1be0ffe5a
malakshaer <shaermk06@gmail.com>
2022-09-21 15:45:56 +0300

    checkEmail.php updated

--- server/sign/checkEmail.php
+++ server/sign/checkEmail.php
@@ -1,19 +1,26 @@
 <?php
- 
-if($_SERVER["REQUEST_METHOD"] == "GET")
-{
-    $mysqli = new mysqli("localhost", "root", "", "spruce_db");
-    
-    $email = isset($_GET["email"]);
 
-    $query = "SELECT * FROM `users` WHERE `email` = $email";
-    
-    $stmt = $mysqli->query($query);
+include_once("../connection.php");
 
-    if($stmt) {
-        echo 'This email address is already used!';
-    }
+$email = isset($_POST['email']);
 
+$query = $mysqli->prepare("SELECT email FROM users WHERE email = ?");
+$query->bind_param('s', $email);
+$query->execute();
+$arr = $query->get_result();
+
+
+$result =[];
+while($value = $arr->fetch_assoc()){
+    $result[] = $value;
+};
+
+// if the result is empty then the given value does not exist in the table
+if(!$result){
+    $response["exist"] = FALSE;
+}else{
+    $response["exist"] = TRUE;
 }
 
+echo json_encode($response);
 ?>
