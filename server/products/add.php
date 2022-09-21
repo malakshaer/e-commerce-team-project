0724939fc6d0421fca59b919fc4ef6174b9155e7
malakshaer <shaermk06@gmail.com>
2022-09-21 22:14:30 +0300

    TASK-22 modifying add product api

--- server/products/add.php
+++ server/products/add.php
@@ -2,28 +2,27 @@
 
 include("../connection.php");
 
-$id = isset($_POST["id"]);
-$name = isset($_POST["name"]);
-$description = isset($_POST["description"]);
-$price =isset($_POST["price"]);
-$discount_price = isset($_POST["discount_price"]);
-$picture_img = isset($_POST["picture_img"]);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $id = $_POST["id"];
+  $name = $_POST["name"];
+  $description = $_POST["description"];
+  $price =$_POST["price"];
+  $discount_price = $_POST["discount_price"];
+  $picture_img = isset($_POST["picture_img"]);
+  
 
+  $sql = "INSERT INTO products(id, name,description,price,discount_price,picture_img) VALUE (?,?,?,?,?,?)";
+  $query =$mysqli->prepare($sql);
 
-$query = "INSERT INTO products(id, name,description,price,discount_price,picture_img) VALUE (?,?,?,?,?,?)";
-$query = $mysqli->prepare($query);
-$query->bind_param("ssssss", $id, $name,$description,$price,$discount_price,$picture_img);
-$query->execute();
+  $query->bind_param("ssssss", $id, $name, $description, $price,$discount_price,$picture_img);
+  $query->execute();
 
+  if($query){
+      echo "done successfully";
 
-if($query)
-{
-  echo "done successfully!";
-
-}
-else
-{
-  echo "Something went wrong!";
+  }else{
+      echo "something went wrong";
+  }
 }
 
 ?>
