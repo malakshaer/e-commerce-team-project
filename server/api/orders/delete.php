1d3a167d6322afe4d5e6a7919104c2fffe43fceb
malakshaer <shaermk06@gmail.com>
2022-09-22 07:56:35 +0300

    TASK-24 delete order api file added

---
+++ server/api/orders/delete.php
@@ -0,0 +1,25 @@
+<?php
+
+include("../connection.php");
+
+$id = isset($_POST["id"]);
+
+
+$query = "DELETE FROM orders WHERE `id` = ?";
+
+$query = $mysqli->prepare($query);
+$query->bind_param("s", $id);
+$query->execute();
+
+
+if($query)
+{
+  echo "done successfully!";
+
+}
+else
+{
+  echo "Something went wrong!";
+}
+
+?>
