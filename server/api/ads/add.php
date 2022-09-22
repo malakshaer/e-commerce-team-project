4b5d118ddeb652722f89578fd5ea41d5c43409c2
malakshaer <shaermk06@gmail.com>
2022-09-22 08:18:47 +0300

    TASK-25 add ads api modified

--- server/api/ads/add.php
+++ server/api/ads/add.php
@@ -5,9 +5,10 @@ include("../connection.php");
 
 $pic_url = $_POST["picture_id"];
 $link = $_POST["link"];
+$seller_id = $_POST["user_types_id"];
+$user_id = $_POST["user_id"];
 
-
-$query = "INSERT INTO `ads` ( picture_url,link) VALUE (?,?)";
+$query = "INSERT INTO `ads` ( picture_url,link) VALUE (?,?) WHERE `users_id` = $seller_id";
 $query = $mysqli->prepare($query);
 $query->execute();
 
