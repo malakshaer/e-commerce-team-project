11b9906e6b9a4aba46608b21b8e6980c6b76b280
malakshaer <shaermk06@gmail.com>
2022-09-22 10:58:01 +0300

    TASK-23 api query is fixed

--- server/api/products/add_favorite.php
+++ server/api/products/add_favorite.php
@@ -2,24 +2,24 @@
 
 include("../../connection.php");
 
-$product_id = $_POST["products_id"];
-$client_id = isset($_POST["user_types_id"]);
-$id = $_POST["id"];
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+  $product_id = $_POST["id"];
+  $client_id = $_POST["user_types_id"];
+
+  $query = "SELECT * FROM products as p,favorites as f WHERE `f.products_id` = `p.id` = $client_id";
+  $query = $mysqli->prepare($query);
+  $query->execute();
 
 
-$query = "SELECT * FROM products as p,favorites as f WHERE `f.products_id` = `p.id` = $client_id";
-$query = $mysqli->prepare($query);
-$query->execute();
+  if($query)
+  {
+    echo "done successfully!";
 
-
-if($query)
-{
-  echo "done successfully!";
-
+  }
+  else
+  {
+    echo "Something went wrong!";
+  }
 }
-else
-{
-  echo "Something went wrong!";
-}
-
 ?>
