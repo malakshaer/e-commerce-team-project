9000e942442abde4fe4cac6564d2424a95441358
malakshaer <shaermk06@gmail.com>
2022-09-22 14:09:29 +0300

    TASK-24 get_user_oreder file fixed

--- server/api/orders/get_user_order.php
+++ server/api/orders/get_user_order.php
@@ -2,29 +2,30 @@
 
 include("../../connection.php");
 
-$id = $_GET["id"];
-$email = isset($_GET['email']);
-$client_id = $_GET["user_types_id"];
+if($id = isset($_GET["id"]) && $seller_id = $_GET["user_types_id"]){
+    
+    $id = $_GET["id"];
+    $client_id = $_GET["user_types_id"];
 
 
 
-$query = "SELECT `o.id` FROM `orders` as o,`users` as u WHERE `o.users_id` = `u.id` =$client_id";
-$query = $mysqli->prepare($query);
-$query->execute();
-$arr = $query->get_result();
+    $query = "SELECT `o.id` FROM `orders` as o,`users` as u WHERE `o.users_id` = `u.id` =$client_id";
+    $query = $mysqli->prepare($query);
+    $query->execute();
+    $arr = $query->get_result();
 
 
-$result =[];
-while($value = $arr->fetch_assoc()){
-    $result[] = $value;
-};
+    $result =[];
+    while($value = $arr->fetch_assoc()){
+        $result[] = $value;
+    };
 
-// if the result is empty then the given value does not exist in the table
-if(!$result){
-    $response["exist"] = FALSE;
-}else{
-    $response["exist"] = TRUE;
+    if(!$result){
+        $response["exist"] = FALSE;
+    }else{
+        $response["exist"] = TRUE;
+    }
+
+    echo json_encode($response);
 }
-
-echo json_encode($response);
 ?>
