b3a8a84f72cc0985e37eb67a82d9c7aaa18765e6
malakshaer <shaermk06@gmail.com>
2022-09-22 07:52:47 +0300

    TASK-23 show all product api file fixed

---
+++ server/api/products/all.php
@@ -0,0 +1,27 @@
+<?php
+
+include("../connection.php");
+
+
+$categories_id = isset($_GET["categories_id"]);
+
+
+$query = "SELECT (name,picture_img) FROM products WHERE `id` = $categories_id";
+$query = $mysqli->prepare($query);
+$query->execute();
+
+$arr = $query->get_result();
+
+$result =[];
+while($value = $arr->fetch_assoc()){
+    $result[] = $value;
+};
+
+if(!$result){
+    $response["exist"] = FALSE;
+}else{
+    $response["exist"] = TRUE;
+}
+
+echo json_encode($response);
+?>
