2249b223d81f306ca71a8fa27c9ddcf7be41f972
malakshaer <shaermk06@gmail.com>
2022-09-22 07:45:19 +0300

    signup.php file fixed

---
+++ server/api/sign/signup.php
@@ -0,0 +1,32 @@
+<?php
+include("./server/connection.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    
+    
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $birthday =$_POST["birthday"];
+
+    $passHash = hash("sha256", $_POST["password"]);
+    $password .= "a";
+    $query = $mysqli->prepare("INSERT INTO users (`name`,`email`, `password`,`birthday`) VALUES (? ,?, ?, ?)");
+    $query->bind_param("ssss", $name, $email,$password,$birthday);
+    $query->execute();
+
+
+    if($query)
+    {
+    echo "Registration done successfully!";
+    
+    }
+    else
+    {
+    echo "Something went wrong while registration!";
+    }
+    
+}
+
+?>
