4d6243ccb228821287021c3302830dc421e794e2
malakshaer <shaermk06@gmail.com>
2022-09-22 08:03:38 +0300

    TASK-23 show favorite items api added

---
+++ server/api/clients/get_favorite.php
@@ -0,0 +1,22 @@
+<?php
+
+   include("../connection.php");
+   
+   if($_SERVER["REQUEST_METHOD"] == "GET")
+   {
+    
+        $client_id = isset($_GET["user_types_id"]);
+
+        $query= "SELECT `f.products_id` FROM `favorites` as f,users as u WHERE `f.users_id`=`u.id` = $client_id";
+        $res = $mysqli -> query($query);
+
+        if($res)
+        {
+        echo "Data inserted into the database successfully!";
+        }
+        else
+        {
+        echo "Something went wrong!<BR>";
+        }
+    }
+?>
