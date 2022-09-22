2708b1174c775b4533a242198882729bd40d16af
malakshaer <shaermk06@gmail.com>
2022-09-22 07:51:58 +0300

    TASK-23 show favorite product api modified

---
+++ server/api/products/add_favorite.php
@@ -0,0 +1,25 @@
+<?php
+
+include("../connection.php");
+
+$product_id = $_POST["products_id"];
+$client_id = isset($_POST["user_types_id"]);
+$id = $_POST["id"];
+
+
+$query = "SELECT * FROM products as p,favorites as f WHERE `f.products_id` = `p.id` = $client_id";
+$query = $mysqli->prepare($query);
+$query->execute();
+
+
+if($query)
+{
+  echo "done successfully!";
+
+}
+else
+{
+  echo "Something went wrong!";
+}
+
+?>
