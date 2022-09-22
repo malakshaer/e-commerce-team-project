8259d7659a3dde47d30622b1da2e0314d3527d9e
malakshaer <shaermk06@gmail.com>
2022-09-22 07:54:12 +0300

    TASK-23 show product api file modified

---
+++ server/api/products/get_product.php
@@ -0,0 +1,30 @@
+<?php
+
+include("../connection.php");
+
+$id = isset($_GET["id"]);
+
+$query = "SELECT * FROM products WHERE `id` = ? ";
+$query = $mysqli->prepare($query);
+$query->bind_param('s', $id);
+$query->execute();
+$arr = $query->get_result();
+
+$arr =[];
+while($value = $array->fetch_assoc()){
+    $arr[] = $value;
+}
+
+$result =[];
+if($arr)
+{
+  echo "done successfully!";
+
+}
+else
+{
+  echo "Something went wrong!";
+}
+echo json_encode($result);
+
+?>
