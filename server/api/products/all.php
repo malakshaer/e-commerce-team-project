92db526a9d5a0db456eda24ca0d5a5ce43f50535
malakshaer <shaermk06@gmail.com>
2022-09-22 10:58:24 +0300

    TASK-23 query is fixed

--- server/api/products/all.php
+++ server/api/products/all.php
@@ -2,26 +2,29 @@
 
 include("../../connection.php");
 
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    
+    $categories_id = isset($_GET["categories_id"]);
 
-$categories_id = isset($_GET["categories_id"]);
 
+    $query = "SELECT * FROM products WHERE `id` = ?";
+    $query = $mysqli->prepare($query);
+    $query->bind_param('s', $categories_id);
+    $query->execute();
 
-$query = "SELECT (name,picture_img) FROM products WHERE `id` = $categories_id";
-$query = $mysqli->prepare($query);
-$query->execute();
+    $arr = $query->get_result();
 
-$arr = $query->get_result();
+    $result =[];
+    while($value = $arr->fetch_assoc()){
+        $result[] = $value;
+    };
 
-$result =[];
-while($value = $arr->fetch_assoc()){
-    $result[] = $value;
-};
+    if(!$result){
+        $response["exist"] = FALSE;
+    }else{
+        $response["exist"] = TRUE;
+    }
 
-if(!$result){
-    $response["exist"] = FALSE;
-}else{
-    $response["exist"] = TRUE;
+    echo json_encode($response);
 }
-
-echo json_encode($response);
 ?>
