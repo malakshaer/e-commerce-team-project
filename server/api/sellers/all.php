bd9095a3344f439f73f4409f35f2b6f8c6b8bd73
malakshaer <shaermk06@gmail.com>
2022-09-22 07:49:00 +0300

    TASK-22 show all sellers api modified

---
+++ server/api/sellers/all.php
@@ -0,0 +1,30 @@
+<?php
+
+include("../connection.php");
+
+$id = isset($_GET["id"]);
+$name = isset($_GET["name"]);
+$profile_img =isset($_GET["profile_img"]);
+$seller_id = isset($_GET["user_type_id"]);
+
+
+$query = "SELECT FROM users(name,profile_img) = (?,?) WHERE `id` = $seller_id";
+$query = $mysqli->prepare($query);
+$query->bind_param("ss", $name,$profile_img);
+$query->execute();
+
+$result =[];
+while($value = $arr->fetch_assoc()){
+    $result[] = $value;
+};
+
+
+if(!$result){
+  $response["exist"] = FALSE;
+}else{
+  $response["exist"] = TRUE;
+}
+
+echo json_encode($response);
+
+?>
