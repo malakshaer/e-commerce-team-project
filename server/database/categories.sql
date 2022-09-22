461c4e5c75b2d31b9941e8670730dab9913e6344
malakshaer <shaermk06@gmail.com>
2022-09-22 23:16:33 +0300

    TASK-44 categories data added

---
+++ server/database/categories.sql
@@ -0,0 +1,15 @@
+insert into categories (id, name, category_date, users_id) values (1, 'Justice', '11/28/2021', 1);
+insert into categories (id, name, category_date, users_id) values (2, 'CLINIMIX E', '8/2/2022', 2);
+insert into categories (id, name, category_date, users_id) values (3, 'Temazepam', '10/6/2021', 3);
+insert into categories (id, name, category_date, users_id) values (4, 'TOPCARE', '1/20/2022', 4);
+insert into categories (id, name, category_date, users_id) values (5, 'Smart Sense Allergy Relief', '10/27/2021', 5);
+insert into categories (id, name, category_date, users_id) values (6, 'Heparin Lock Flush', '4/7/2022', 6);
+insert into categories (id, name, category_date, users_id) values (7, 'Allergy Relief', '11/5/2021', 7);
+insert into categories (id, name, category_date, users_id) values (8, 'Diovan', '4/2/2022', 8);
+insert into categories (id, name, category_date, users_id) values (9, 'Testosterone', '1/31/2022', 9);
+insert into categories (id, name, category_date, users_id) values (10, 'IRBESARTAN', '6/23/2022', 10);
+insert into categories (id, name, category_date, users_id) values (11, 'Tartar Control Plus', '4/16/2022', 11);
+insert into categories (id, name, category_date, users_id) values (12, 'Cell Salts', '1/25/2022', 12);
+insert into categories (id, name, category_date, users_id) values (13, 'Sotalol Hydrochloride', '2/20/2022', 13);
+insert into categories (id, name, category_date, users_id) values (14, 'DPT TOX', '11/26/2021', 14);
+insert into categories (id, name, category_date, users_id) values (15, 'Losartan Potassium', '5/16/2022', 15);
