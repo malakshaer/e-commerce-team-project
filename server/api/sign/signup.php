4b89145b5c4ef213490990c8ab90679ef72a4713
malakshaer <shaermk06@gmail.com>
2022-09-22 07:47:36 +0300

    TASK-15 sign folder modified

--- server/api/sign/signup.php
+++ server/api/sign/signup.php
@@ -3,8 +3,6 @@ include("./server/connection.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    
-    
     $name = $_POST["name"];
     $email = $_POST["email"];
     $password = $_POST["password"];
