24eb23923ed86000fe56c9db0069de6759da74c8
malakshaer <shaermk06@gmail.com>
2022-09-22 14:06:54 +0300

    TASK-24 get_product_oreder file fixed

--- server/api/orders/get_active_order.php
+++ server/api/orders/get_active_order.php
@@ -2,28 +2,28 @@
 
 include("../../connection.php");
 
-$id = $_GET["id"];
-$email = isset($_GET['email']);
-$seller_id = $_GET["user_types_id"];
+if($id = isset($_GET["id"]) && $seller_id = $_GET["user_types_id"]){
+
+    $id = $_GET["id"];
+    $seller_id = $_GET["user_types_id"];
+
+    $query = "SELECT `o.id` FROM `orders` as o,`users` as u WHERE `o.users_id` = `u.id` =$seller_id";
+    $query = $mysqli->prepare($query);
+    $query->execute();
+    $arr = $query->get_result();
 
 
+    $result =[];
+    while($value = $arr->fetch_assoc()){
+        $result[] = $value;
+    };
 
-$query = "SELECT `o.id` FROM `orders` as o,`users` as u WHERE `o.users_id` = `u.id` =$seller_id";
-$query = $mysqli->prepare($query);
-$query->execute();
-$arr = $query->get_result();
+    if(!$result){
+        $response["exist"] = FALSE;
+    }else{
+        $response["exist"] = TRUE;
+    }
 
-
-$result =[];
-while($value = $arr->fetch_assoc()){
-    $result[] = $value;
-};
-
-if(!$result){
-    $response["exist"] = FALSE;
-}else{
-    $response["exist"] = TRUE;
+    echo json_encode($response);
 }
-
-echo json_encode($response);
 ?>
