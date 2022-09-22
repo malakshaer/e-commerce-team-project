fedf40c81ba2df8722df3b55bf373b53c990fe4a
malakshaer <shaermk06@gmail.com>
2022-09-22 07:59:02 +0300

    TASK-25 send message api added

---
+++ server/api/messages/send.php
@@ -0,0 +1,27 @@
+<?php
+include('../connection.php');
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $message = $_POST["message"];
+    $sender_id = isset($_POST["sender_id"]);
+    $receiver_id = isset($_POST["receiver_id"]);
+    $message_date = $_POST["message_date"];
+    
+
+    $sql = "INSERT INTO messages(receiver_id, sender_id,message_date,message) VALUE (?,?,?,?)";
+    $query =$mysqli->prepare($sql);
+
+    $query->bind_param("ssss", $receiver_id, $sender_id, $message_date, $message);
+    $query->execute();
+
+    if($query){
+        echo "done successfully";
+
+    }else{
+        echo "something went wrong";
+    }
+}
+
+
+?>
