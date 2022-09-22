8a0d689ce55760b1b7855b99bffc2211c74c33f6
malakshaer <shaermk06@gmail.com>
2022-09-22 07:50:37 +0300

    TASK-22 get single seller api file modified

---
+++ server/api/sellers/get_seller.php
@@ -0,0 +1,33 @@
+<?php
+
+include("../connection.php");
+
+
+$name = isset($_GET["name"]);
+$email = isset($_GET["email"]);
+$birthday =isset($_GET["birthday"]);
+$join_date =isset($_GET["join_date"]);
+$profile_img =isset($_GET["profile_img"]);
+$seller_id = isset($_GET["user_type_id"]);
+
+$query = "SELECT * FROM users  WHERE `id` = $seller_id";
+$query = $mysqli->prepare($query);
+$query->bind_param('s', $id);
+$query->execute();
+$arr = $query->get_result();
+
+
+$result =[];
+while($value = $arr->fetch_assoc()){
+    $result[] = $value;
+};
+
+if(!$result){
+    $response["exist"] = FALSE;
+}else{
+    $response["exist"] = TRUE;
+}
+
+echo json_encode($response);
+
+?>
