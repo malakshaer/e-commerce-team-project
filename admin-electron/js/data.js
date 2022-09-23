250cec8a0dec02b03d79c16c746ee17bb56238f3
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-23 14:58:09 +0300

    TASK-51 fixing tabs view

--- admin-electron/js/data.js
+++ admin-electron/js/data.js
@@ -2,62 +2,99 @@
 const sellers = [
   {
     id: 1,
-    name: "Meryl Roberson",
-    img_url: "https://robohash.org/undeeteaque.png?size=50x50&set=set1",
-    username: "mroberson0",
+    name: "Jean Gamett",
+    img_url: "https://robohash.org/placeatdolorequod.png?size=300x300&set=set1",
     is_ban: true,
   },
   {
     id: 2,
-    name: "Bennett Kollas",
-    img_url: "https://robohash.org/fugaiureat.png?size=50x50&set=set1",
-    username: "bkollas1",
+    name: "Jessica Miliffe",
+    img_url:
+      "https://robohash.org/nostrumrerumcommodi.png?size=300x300&set=set1",
+    is_ban: false,
   },
   {
     id: 3,
-    name: "Yves Pitherick",
-    img_url:
-      "https://robohash.org/architectoreiciendiset.png?size=50x50&set=set1",
-    username: "ypitherick2",
+    name: "Giuditta Peagrim",
+    img_url: "https://robohash.org/itaqueiuresed.png?size=300x300&set=set1",
+    is_ban: true,
   },
   {
     id: 4,
-    name: "Gale Gillbard",
-    img_url: "https://robohash.org/iustoundeest.png?size=50x50&set=set1",
-    username: "ggillbard3",
+    name: "Brock Thurner",
+    img_url: "https://robohash.org/ducimusenimsed.png?size=300x300&set=set1",
+    is_ban: false,
   },
   {
     id: 5,
-    name: "Rosalinde Truelock",
-    img_url: "https://robohash.org/quimagniaut.png?size=50x50&set=set1",
-    username: "rtruelock4",
+    name: "Kaiser Gozzett",
+    img_url: "https://robohash.org/atquaseos.png?size=300x300&set=set1",
+    is_ban: false,
   },
   {
-    id: 3,
-    name: "Yves Pitherick",
+    id: 6,
+    name: "Fancy Bridgement",
     img_url:
-      "https://robohash.org/architectoreiciendiset.png?size=50x50&set=set1",
-    username: "ypitherick2",
+      "https://robohash.org/dolorumtemporaporro.png?size=300x300&set=set1",
+    is_ban: true,
   },
   {
-    id: 4,
-    name: "Gale Gillbard",
-    img_url: "https://robohash.org/iustoundeest.png?size=50x50&set=set1",
-    username: "ggillbard3",
+    id: 7,
+    name: "Rice Zealander",
+    img_url: "https://robohash.org/quamutminima.png?size=300x300&set=set1",
+    is_ban: true,
   },
   {
-    id: 5,
-    name: "Rosalinde Truelock",
-    img_url: "https://robohash.org/quimagniaut.png?size=50x50&set=set1",
-    username: "rtruelock4",
+    id: 8,
+    name: "Junette Leake",
+    img_url: "https://robohash.org/atautalias.png?size=300x300&set=set1",
+    is_ban: false,
+  },
+  {
+    id: 9,
+    name: "Red Eynon",
+    img_url: "https://robohash.org/accusantiumautquo.png?size=300x300&set=set1",
+    is_ban: true,
+  },
+  {
+    id: 10,
+    name: "Theodore Tripon",
+    img_url: "https://robohash.org/veniamquiaomnis.png?size=300x300&set=set1",
+    is_ban: true,
   },
 ];
 
 const clients = [
   {
+    id: 1,
+    name: "Jacqui Becconsall",
+    img_url: "https://robohash.org/fugitquaeofficia.png?size=300x300&set=set1",
+    is_ban: true,
+  },
+  {
+    id: 2,
+    name: "Lorne Keirle",
+    img_url:
+      "https://robohash.org/velmaioresaspernatur.png?size=300x300&set=set1",
+    is_ban: false,
+  },
+  {
+    id: 3,
+    name: "Carlyn Tilt",
+    img_url: "https://robohash.org/fugaeaquetempore.png?size=300x300&set=set1",
+    is_ban: false,
+  },
+  {
     id: 4,
-    name: "Gale Gillbard",
-    img_url: "https://robohash.org/iustoundeest.png?size=50x50&set=set1",
-    username: "ggillbard3",
+    name: "Oona Welford",
+    img_url:
+      "https://robohash.org/corporisiurepraesentium.png?size=300x300&set=set1",
+    is_ban: true,
+  },
+  {
+    id: 5,
+    name: "Alva Greenroad",
+    img_url: "https://robohash.org/assumendaetqui.png?size=300x300&set=set1",
+    is_ban: false,
   },
 ];
