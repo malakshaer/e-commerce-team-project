a88f000c1dc29e2ac523f9b6bd9a3bee598ee4be
malakshaer <shaermk06@gmail.com>
2022-09-24 18:05:59 +0300

    TASK-23 get all products api added

---
+++ server/api/products/get_all_products.php
@@ -0,0 +1,29 @@
+<?php
+
+include("../../connection.php");
+
+
+    
+    
+
+    $query = $mysqli->prepare("SELECT * FROM products ");
+    
+    $query->execute();
+
+    $array = $query->get_result();
+
+    $response =[];
+
+    while($value = $array->fetch_assoc()){
+        $response[] = $value;
+    };
+
+    if($response){
+        echo json_encode($response); 
+    }else{
+        echo "error";
+    }
+
+    
+// }
+?>
