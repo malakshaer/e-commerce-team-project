f62654d0286c8d935ba01fd1a94de58985775398
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-25 19:48:54 +0300

    TASK-55 adding sellers in admin

--- admin-electron/js/popAddUser.js
+++ admin-electron/js/popAddUser.js
@@ -1,3 +1,6 @@
+const addSellerAPI =
+  "http://localhost/e-commerce-team-project/server/api/sellers/add.php";
+
 const popAddUser = () => {
   return `
   <div class="pop-up">
@@ -6,10 +9,10 @@ const popAddUser = () => {
       <div class="close-pop-up" id="close-pop-up">x</div>
     </div>
   <div class='pop-body'>
-    <input placeholder='Email'/>
-    <input placeholder='Name'/>
-    <input placeholder='Password'/>
-    <button class="sign-btn">Add</div>
+    <input placeholder='Email' id='seller-email'/>
+    <input placeholder='Name' id='seller-name'/>
+    <input placeholder='Password' id='seller-pass'/>
+    <button class="sign-btn" id='add-one'>Add</div>
   </div>
   </div>
   </div>`;
@@ -24,6 +27,27 @@ const setpopUp = () => {
     document.getElementById("pop-up").classList.remove("display-none");
     document.getElementById("close-pop-up").onclick = () => {
       document.getElementById("pop-up").classList.add("display-none");
+    };
+
+    // on click add seller
+
+    document.getElementById("add-one").onclick = () => {
+      var params = new URLSearchParams();
+      params.append("email", document.getElementById("seller-email").value);
+      params.append("name", document.getElementById("seller-pass").value);
+      params.append("password", document.getElementById("seller-name").value);
+      params.append(
+        "profile_img",
+        "https://robohash.org/recusandaeinciduntet.png?size=250x250&set=set1"
+      );
+
+      axios
+        .post(addSellerAPI, params)
+        .then(
+          (res) =>
+            res.data.id &&
+            document.getElementById("pop-up").classList.add("display-none")
+        );
     };
   };
 };
